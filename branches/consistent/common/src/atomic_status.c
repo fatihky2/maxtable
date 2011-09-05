246ba335e991fa8e1c20d38cb1e24a126e98d6c2
baoshengdeer@gmail.com <baoshengdeer@gmail.com@5a980f27-13a2-20b2-82b5-70f809bb7fcd>
2011-09-05 08:37:29 +0000

    Implement consistent I/O engine, for real-time data storage.

---
+++ branches/consistent/common/src/atomic_status.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+
+
+#include "atomic_status.h"
+#include "buffer.h"
+
+int change_status(BUF *buffer, int curr, int dest)
+{
+	int count = 0;
+    while(!__sync_bool_compare_and_swap(&(buffer->bsstab->atomic_stat), curr, dest))
+    {
+    	count ++;
+        //usleep(100);
+    }
+    return count;
+}
+
+void change_value_add(int *src, int delta)
+{
+    __sync_fetch_and_add(src, delta);
+}
+
+void change_value_sub(int *src, int delta)
+{
+    __sync_fetch_and_sub(src, delta);
+}
+
