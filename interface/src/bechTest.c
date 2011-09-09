0e8865096600f36d92f00d2dda3afd5f9b59bac2
yingfei.xue <yingfei.xue@gmail.com>
2011-09-09 09:18:04 +0000

    Add new file for the benchmark work.

---
+++ interface/src/bechTest.c
@@ -0,0 +1,76 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/time.h>
+#include "interface.h"
+
+struct timeval tpStart;
+struct timeval tpEnd;
+float timecost;
+
+
+#ifdef MAXTABLE_BENCH_TEST
+int main()
+{
+	conn * connection;
+
+
+	if(cli_connection("172.16.10.11", 1959, &connection))
+	{
+		printf("hello\n");
+		char resp[256], cmd[256];
+		int i, len;
+if(0)
+{
+		memset(resp, 0, 256);
+		sprintf(cmd, "create table gu(id1 varchar, id2 int, id3 varchar)");
+		cli_commit(connection, cmd, resp, &len);
+		printf("ret: %s\n", resp);
+
+		gettimeofday(&tpStart, NULL);
+
+
+		for(i = 1; i < 100000; i ++)
+		{
+			memset(resp, 0, 256);
+			memset(cmd, 0, 256);
+			sprintf(cmd, "insert into gu(gggg%d, %d, bbbb%d)", i, i, i);
+
+			cli_commit(connection, cmd, resp, &len);
+		}
+		
+		gettimeofday(&tpEnd, NULL);
+		timecost = 0.0f;
+		timecost = tpEnd.tv_sec - tpStart.tv_sec + (float)(tpEnd.tv_usec-tpStart.tv_usec)/1000000;
+		printf("Inserted rows = %d\n", i);
+		printf("time cost: %f\n", timecost);
+
+
+}//if(0)
+
+
+		gettimeofday(&tpStart, NULL);
+
+		for(i = 1; i < 5000; i ++)
+		{
+			memset(resp, 0, 256);
+			memset(cmd, 0, 256);
+			sprintf(cmd, "select gu(gggg%d)", i);
+
+			cli_commit(connection, cmd, resp, &len);
+		}
+	
+		gettimeofday(&tpEnd, NULL);
+		timecost = 0.0f;
+		timecost = tpEnd.tv_sec - tpStart.tv_sec + (float)(tpEnd.tv_usec-tpStart.tv_usec)/1000000;
+		
+		printf ("selected rows = %d \n", i);
+		printf("time cost: %f\n", timecost);
+	
+		cli_exit(connection);
+	}
+	
+	return 0;
+}
+#endif
+
