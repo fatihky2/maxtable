727d8d66e50a123c6b0765df0ce5ef9c9d7605bc
yingfei.xue@gmail.com <yingfei.xue@gmail.com@5a980f27-13a2-20b2-82b5-70f809bb7fcd>
2011-09-15 02:54:01 +0000

    Update the test case.

--- trunk/stresstest/insert.sh
+++ trunk/stresstest/insert.sh
@@ -1,9 +1,7 @@
-date
-./sample_10000 &
-./sample_20000 &
-./sample_30000 &
-./sample_40000 &
-./sample_50000 &
-date
+./sample_ins_10000 &
+./sample_ins_20000 &
+./sample_ins_30000 &
+./sample_ins_40000 &
+./sample_ins_50000 &
 
 
